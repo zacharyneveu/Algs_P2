d04eee8c3bb1693a926fae992cb208b12505d18e
Zachary Neveu <Zachary Neveu>
2017-02-04 13:22:37 -0500

    Add reconstructDeck function

--- NevDec2Master/deck.cpp
+++ NevDec2Master/deck.cpp
@@ -4,6 +4,7 @@
 #include <algorithm>
 #include "deck.h"
 #include "card.h"
+#include "d_node.h"
 
 
 //the deconstructDeck function takes the linked list deck and converts it
@@ -16,7 +17,7 @@ vector<node<card>> deck::deconstructDeck() {
 	vectorDeck.push_back(*head);
 
 	//for loop iterates over all cards except for original head
-	for (int i=1; i<52; i++)
+	while(head->next!=NULL)
 	{
 		//advance head through linked list
 		head = head->next;
@@ -27,11 +28,19 @@ vector<node<card>> deck::deconstructDeck() {
 	return vectorDeck;
 }
 
+void deck::reconstructDeck(vector<node<card>> cards) {
+	for (unsigned int i=0; i<cards.size()-1; i++)
+	{
+		cards[i].next = &cards[i+1];
+	}
+	cards[cards.size()-1].next=NULL;
+}
+
 //The shuffle function uses the random_shuffle function from the
 //algorithms library to randomly re-order the deck of cards
 void deck::shuffle() {
 	//Create vector out of the deck
-	vector<node> deck = deconstructDeck();
+	vector<node<card>> deck = deconstructDeck();
 	//call shuffle function on vector
 	random_shuffle(deck.begin(), deck.end());
 	//reconstruct deck from vector
